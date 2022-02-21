5782a8df9446a35402f894952f9592e8bd492b90
laruh <oblomoff616@gmail.com>
2022-02-21 22:19:31 +0700

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,48 @@
+package ru.job4j.pool;
+
+import java.util.Objects;
+
+public class User {
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "username='" + username
+                + '\''
+                + ", email='" + email
+                + '\''
+                + '}';
+    }
+}
