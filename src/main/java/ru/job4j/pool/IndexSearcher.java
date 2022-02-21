1bb9ba4d4ab6ea508b18bd43c9085e8fe0c5e31c
laruh <oblomoff616@gmail.com>
2022-02-22 02:26:44 +0700

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pool/IndexSearcher.java
@@ -0,0 +1,48 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class IndexSearcher<T> extends RecursiveTask<Integer> {
+    private final T[] array;
+    private final int fromInd;
+    private final int toInd;
+    private final T elem;
+
+    public IndexSearcher(T[] array, int fromInd, int toInd, T elem) {
+        this.array = array;
+        this.fromInd = fromInd;
+        this.toInd = toInd;
+        this.elem = elem;
+    }
+
+    public int findIndex(T[] array, int fromInd, int toInd, T elem) {
+        int rsl = -1;
+        for (int i = fromInd; i <= toInd; i++) {
+            if (elem.equals(array[i])) {
+                rsl = i;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (fromInd - toInd < 10) {
+           return findIndex(array, fromInd, toInd, elem);
+        }
+        int mid = (fromInd + toInd) / 2;
+        IndexSearcher<T> leftSearch = new IndexSearcher(array, fromInd, mid, elem);
+        IndexSearcher<T> rightSearch = new IndexSearcher(array, mid + 1, toInd, elem);
+        leftSearch.fork();
+        rightSearch.fork();
+        int left = leftSearch.join();
+        int right = rightSearch.join();
+        return findIndex(array, left, right, elem);
+    }
+
+    public static <T> int find(T[] array, T elem) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new IndexSearcher<>(array, 0, array.length, elem));
+    }
+}
