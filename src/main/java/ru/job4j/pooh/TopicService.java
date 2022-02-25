fe3580e5e85bd09e7efbb503bf35d1477a22988a
laruh <oblomoff616@gmail.com>
2022-02-25 23:25:30 +0700

    2. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,14 +1,39 @@
 package ru.job4j.pooh;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TopicService implements Service {
     private final ConcurrentHashMap<String, ConcurrentHashMap<String,
-            ConcurrentLinkedQueue<String>>> topics = new ConcurrentHashMap();
+            ConcurrentLinkedQueue<String>>> topics = new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
+        String type = req.httpRequestType();
+        String nameTopic = req.getSourceName();
+        String param = req.getParam();
+        if ("GET".equals(type)) {
+            topics.putIfAbsent(param, new ConcurrentHashMap<>());
+            ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> topic = topics.get(param);
+            if (topic.get(nameTopic) == null) {
+                topic.putIfAbsent(nameTopic, new ConcurrentLinkedQueue<>());
+                return new Resp("", "200");
+            } else {
+                return new Resp(topic.get(nameTopic).poll(), "200");
+            }
+        }
+        if ("POST".equals(type)) {
+            boolean success = false;
+            for (Map.Entry<String, ConcurrentHashMap<String,
+                    ConcurrentLinkedQueue<String>>> entry : topics.entrySet()) {
+                if (entry.getValue().containsKey(nameTopic)) {
+                    entry.getValue().get(nameTopic).offer(param);
+                    success = true;
+                }
+            }
+            return success ? new Resp(param, "200") : new Resp("", "204");
+        }
         return null;
     }
 }
--- src/test/java/ru/job4j/pooh/TopicServiceTest.java
+++ src/test/java/ru/job4j/pooh/TopicServiceTest.java
@@ -12,17 +12,30 @@ public class TopicServiceTest {
     public void whenTopic() {
         TopicService topicService = new TopicService();
         String paramForPublisher = "temperature=18";
+        String paramForPublisher1 = "temperature=42";
         String paramForSubscriber1 = "client407";
         String paramForSubscriber2 = "client6565";
+        String paramForSubscriber3 = "client3888";
         topicService.process(
                 new Req("GET", "topic", "weather", paramForSubscriber1));
+        topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber3));
         topicService.process(
                 new Req("POST", "topic", "weather", paramForPublisher));
         Resp result1 = topicService.process(
                 new Req("GET", "topic", "weather", paramForSubscriber1));
         Resp result2 = topicService.process(
                 new Req("GET", "topic", "weather", paramForSubscriber2));
+        Resp result3 = topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber3));
+        Resp result4 = topicService.process(
+                new Req("POST", "topic", "weather", paramForPublisher1));
+        Resp result5 = topicService.process(
+                new Req("POST", "topic", "wind", paramForPublisher1));
         assertThat(result1.text(), is("temperature=18"));
         assertThat(result2.text(), is(""));
+        assertThat(result3.text(), is("temperature=18"));
+        assertThat(result4.text(), is("temperature=42"));
+        assertThat(result5.status(), is("204"));
     }
 }
