7e5c01c3696a5307e9ca448560b36050fb21dc00
laruh <oblomoff616@gmail.com>
2022-02-26 00:26:39 +0700

    2. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/test/java/ru/job4j/pooh/TopicServiceTest.java
+++ src/test/java/ru/job4j/pooh/TopicServiceTest.java
@@ -1,11 +1,10 @@
 package ru.job4j.pooh;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-@Ignore
+
 public class TopicServiceTest {
 
     @Test
